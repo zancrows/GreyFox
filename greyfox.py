c4b523e22e4d308bca5becf59482e2ef4ba29942
zancrows <zancrows@tutanota.com>
2019-07-10 21:10:56 +0200

    version numpy simplifié

--- greyfox.py
+++ greyfox.py
@@ -1,30 +1,27 @@
 # coding: utf-8
 # python 3.7.3 x86_64
 
+import binascii
+import numpy as np
+from PIL import Image
+from datetime import datetime
+from itertools import chain, islice
+from abc import ABCMeta, abstractmethod
+from colorama import init, Fore
+
 
 """
 Copyright 2019 Maillard Alexandre
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-
-import binascii
-from PIL import Image
-from datetime import datetime
-from itertools import chain, islice
-from abc import ABCMeta, abstractmethod
-from colorama import init, Fore
 
 
 ############################## functions #######################################
@@ -56,99 +53,43 @@ def iter_by_blockN(iterable, len_bloc=8, format=tuple):
 
 def str_to_bin(string:str) -> str:
     if string:
-        bits = bin(int(binascii.hexlify(bytes(string, "utf8")), 16))[2:]
+        bits = np.binary_repr(int(string.encode("utf8").hex(), 16))
         return bits.zfill(8 * ((len(bits) + 7) // 8))
     return ""
 
 def bin_to_str(sbin:str) -> bytes:
     b_str = b""
-    for i in iter_by_blockN(sbin):
-        binary = "".join(i)
-        integer = int(binary, 2)
-        hexa = f"{integer:02x}"
-        b_str += binascii.unhexlify(hexa)
+    for b in iter_by_blockN(sbin):
+        hexa = np.base_repr(int("".join(b), 2), base=16)
+        b_str += binascii.unhexlify(f"{hexa:>02}")
     return b_str
 
+def bit_editor(ibyte:int, bit:int, mask:int) -> int:
+        return (((ibyte >> (mask+1) << 1) | bit) << mask) | (ibyte & ((1 << mask) - 1))
+
+def extract_bit(ibyte:int, mask:int) -> str:
+        return str((ibyte >> mask) & 1)
+
 ############################## class ###########################################
-
-class PixelLSB:
-    def __init__(self, coor:tuple, color:tuple):
-        self.coor = coor
-        self.color = color
-        self._select_color = None
-
-    @property
-    def color(self) -> tuple:
-        return tuple(self._color)
-
-    @color.setter
-    def color(self, color:tuple) -> None:
-        self._color = list(color)
-
-    def __lshift__(self, other:int):
-        if self._select_color is not None:
-            self._color[self._select_color] <<= other
-            self.color = self._color
-        else:
-            self.color = [c << other for c in self.color]
-        return self
-
-    def __rshift__(self, other:int):
-        if self._select_color is not None:
-            self._color[self._select_color] >>= other
-            self.color = self._color
-        else:
-            self.color = [c >> other for c in self.color]
-        return self
-
-    def __or__(self, other:int):
-        if self._select_color is not None:
-            self._color[self._select_color] |= other
-        else:
-            self.color = [c | other for c in self.color]
-        return self
-
-    def __and__(self, other:int):
-        if self._select_color is not None:
-            self._color[self._select_color] &= other
-        else:
-            self.color = [c & other for c in self.color]
-        return self
-
-    def __getitem__(self, idx:int):
-        self._select_color = idx
-        return self
-
-    def __repr__(self):
-        return f"{self.coor}, {self.color}"
-
-    def extract_bit(self, mask:tuple=(0,)) -> str:
-        if self._select_color is not None:
-            bit = ""
-            for i in mask:
-                bit += str((self.color[self._select_color] >> i) & 1)
-            return bit
-        else:
-            raise IndexError("Index is needed for use this method")
-
 
 class StrategyLSB(metaclass=ABCMeta):
     @abstractmethod
     def action(self, absi, ordo, colors):
         raise NotImplementedError
 
-    @classmethod
-    def get_pixel(cls, img:Image, absi:int, ordo:int):
-        for y in ordo:
-            for x in absi:
-                yield PixelLSB((x, y), img.getpixel((x, y)))
-
 
 class EmbededStrategyLSB(StrategyLSB):
-    def action(self, absi:range, ordo:range, colors:dict, params_strategy:dict) -> None:
+    def action(self, absi:slice, ordo:slice, colors:dict, params_strategy:dict) -> None:
         data_to_embeded = params_strategy.get("data_to_embeded")
+        mask = params_strategy.get("mask", {})
+        repr_mask = mask if mask else "Default mask -> (0,)"
+        self.logger.send(("info", f"Mask -> {repr_mask}"))
         file_name_ = params_strategy.get('file_name', self.file_name)
         file_name = f"hidden_{file_name_}"
+        width, height = len(self.array_image[0, absi]), len(self.array_image[ordo])
+
+        if self.mode == "L" or self.mode == "P":
+            self.array_image.shape = (height, width, 1)
 
         if not data_to_embeded:
             err_msg = "data_to_embeded is empty"
@@ -159,30 +100,39 @@ class EmbededStrategyLSB(StrategyLSB):
         bits = list(str_to_bin(data_to_embeded))
         self.logger.send(("info", f"Data to embeded -> {data_to_embeded}"))
 
-        for pixel in StrategyLSB.get_pixel(self.image, absi, ordo):
-            for k_color, v_color in colors.items():
-                if bits:
-                    ((pixel[v_color] >> 1) << 1) | int(bits[0])
-                    bits.pop(0)
-            self.image.putpixel(pixel.coor, pixel.color)
-            if not bits:
-                self.logger.send(("info", f"End embeded here {pixel.coor}"))
-                break
+        for _ordo in self.array_image[ordo]:
+            for _absi in _ordo[absi]:
+                for k_color, v_color in colors.items():
+                    for m in mask.get(k_color, (0,)):
+                        if bits:
+                            _absi[v_color] = bit_editor(_absi[v_color], int(bits[0]), m)
+                            bits.pop(0)
 
+        self.logger.send(("info", f"End embeded "))
         self.logger.send(("info", f"Save file with hidden data -> {file_name}"))
-        self.image.save(file_name)
+
+        if self.mode == "L" or self.mode == "P":
+            self.array_image.shape = (height, width,)
+
+        Image.fromarray(self.array_image, mode=self.mode).save(file_name)
 
 
 class ExtractStrategyLSB(StrategyLSB):
-    def action(self, absi:range, ordo:range, colors:dict, params_strategy:dict) -> None:
-        extract = ""
+    def action(self, absi:slice, ordo:slice, colors:dict, params_strategy:dict) -> None:
         mask = params_strategy.get("mask", {})
         repr_mask = mask if mask else "Default mask -> (0,)"
         self.logger.send(("info", f"Mask -> {repr_mask}"))
+        width, height = len(self.array_image[0, absi]), len(self.array_image[ordo])
 
-        for pixel in StrategyLSB.get_pixel(self.image, absi, ordo):
-            for k_color, v_color in colors.items():
-                extract += pixel[v_color].extract_bit(mask.get(k_color, (0,)))
+        if self.mode == "L" or self.mode == "P":
+            self.array_image.shape = (height, width, 1)
+
+        extract = ""
+        for _ordo in self.array_image[ordo]:
+            for _absi in _ordo[absi]:
+                for k_color, v_color in colors.items():
+                    for m in mask.get(k_color, (0,)):
+                        extract += extract_bit(_absi[v_color], m)
 
         with open("binary.txt", mode="w") as fp:
             self.logger.send(("info", "File binary.txt write"))
@@ -193,50 +143,44 @@ class ExtractStrategyLSB(StrategyLSB):
 
 
 class DetectStrategyLSB(StrategyLSB):
-    def action(self, absi:range, ordo:range, colors:dict, params_strategy:dict) -> None:
+    def action(self, absi:slice, ordo:slice, colors:dict, params_strategy:dict) -> None:
         all_color = params_strategy.get("detect_all_color", False)
         nbr_color = len(colors)
-        width = len(absi)
-        height = len(ordo)
-        show = params_strategy.get("show", True)
-        save = params_strategy.get("save", False)
         file_name_ = params_strategy.get('file_name', self.file_name)
         file_name = f"detect_{file_name_}"
+        vfunc_detect = np.vectorize(lambda x, d: 255 if ((x << d) & 128) else 0)
+        width, height = len(self.array_image[0, absi]), len(self.array_image[ordo])
 
-        if all_color:
-            self.logger.send(("info", f"All color -> Yes"))
-            new_size = (width*7+6, height * (nbr_color+1) + nbr_color)
-            mode, c = "RGB", 0
-            if self.nbr_color_pixel == 4:
-                mode, c = "RGBA", (255, 255, 255)
+        if self.mode == "L" or self.mode == "P":
+            new_size = (width*7+6, height)
+            self.array_image.shape = (height, width, 1)
         else:
-            self.logger.send(("info", f"All color -> No"))
-            new_size = (width*7+6, height * nbr_color + (nbr_color-1))
-        img_detect = Image.new("RGB", new_size, (0, 0, 0))
+            if all_color:
+                self.logger.send(("info", f"All color -> Yes"))
+                new_size = (width*7+6, height * (nbr_color+1) + nbr_color)
+            else:
+                self.logger.send(("info", f"All color -> No"))
+                new_size = (width*7+6, height * nbr_color + (nbr_color-1))
+        img_detect = Image.new(self.mode, new_size)
 
         start = datetime.now()
-        for i, j in enumerate(range(0, new_size[0], self.width), 1):
+        for i, j in enumerate(range(0, new_size[0], width), 1):
             for k, v_color in enumerate(colors.values()):
-                dimension = (i+j, self.height*k+k)
-                new_img = Image.new("L", self.image.size, 0)
-                for pixel in StrategyLSB.get_pixel(self.image, absi, ordo):
-                    (pixel[v_color] << i) & 128
-                    pixel.color = (255,) if pixel.color[v_color] else (0,)
-                    new_img.putpixel(pixel.coor, pixel.color)
+                dimension = (i+j, height*k+k)
+                m_img = vfunc_detect(self.array_image[:, :, v_color], i)
+                new_img = Image.fromarray(m_img[ordo, absi])
                 img_detect.paste(new_img, dimension)
             if all_color:
-                new_img = Image.new(mode, self.image.size, c)
-                dimension = (i+j, dimension[1] + self.height + (k+1))
-                for pixel in StrategyLSB.get_pixel(self.image, absi, ordo):
-                    (pixel << i) & 128
-                    new_img.putpixel(pixel.coor, pixel.color)
+                dimension = (i+j, dimension[1] + height + (k+1))
+                new_img = Image.fromarray((self.array_image << i) & 128)
+
                 img_detect.paste(new_img, dimension)
         end = datetime.now()
         self.logger.send(("info", f"Detect traitement time -> {end - start}"))
 
-        if show:
+        if params_strategy.get("show", True):
             img_detect.show()
-        if save:
+        if params_strategy.get("save", False):
             start_save = datetime.now()
             img_detect.save(file_name)
             end_save = datetime.now()
@@ -246,11 +190,12 @@ class DetectStrategyLSB(StrategyLSB):
 class ImageLSB():
     def __init__(self, image:Image, strategy_lsb:StrategyLSB=None):
         self.image = image
+        self.array_image = np.array(self.image)
         self.width, self.height = self.image.size
         self.strategy_lsb = strategy_lsb
         # /!\ attention 'filename' indique le chemin  absolu de l'image /!\
         self.file_name = self.image.filename
-        self.nbr_color_pixel = len(self.image.getpixel((0,0))) #Ugly :(
+        self.mode = "".join(self.image.getbands())
 
     @property
     def image(self) -> Image.Image:
@@ -277,21 +222,22 @@ class ImageLSB():
             "extract": ExtractStrategyLSB,
             "embeded": EmbededStrategyLSB
         }
-
         self._strategy_lsb = strategy.get(strategy_lsb, strategy_lsb)
 
     def color_sequence(self, custom:tuple=None) -> dict:
         colors = {"RED": 0 , "GREEN": 1, "BLUE": 2}
 
-        if  self.nbr_color_pixel == 4:
+        if  self.mode == "RGBA":
             colors["ALPHA"] = 3
+        elif self.mode == "L" or self.mode == "P":
+            colors = {"GREY": 0}
         if custom:
             colors = {c: colors[c] for c in custom}
         return  colors
 
     def apply_strategy(self, coor:dict={}, color_seq:tuple=None, params_strategy:dict={}) -> None:
-        absi = range(*coor["x"]) if coor.get("x") else range(self.width)
-        ordo = range(*coor["y"]) if coor.get("y") else range(self.height)
+        absi = slice(*coor["x"]) if coor.get("x") else slice(None)
+        ordo = slice(*coor["y"]) if coor.get("y") else slice(None)
         colors = self.color_sequence(color_seq)
         repr_colors = " ".join(colors.keys())
         self.logger = logger(params_strategy.get("verbose", True))
