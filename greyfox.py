98e3be6db9c83fb341bce484718f39f9ce407bd0
zancrows <zancrows@protonmail.com>
2021-02-15 13:17:57 +0100

    Update greyfox.py
    changement d'année pour la licence

--- greyfox.py
+++ greyfox.py
@@ -11,7 +11,7 @@ from colorama import init, Fore
 
 
 """
-Copyright 2020 Maillard Alexandre
+Copyright 2021 Maillard Alexandre
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
